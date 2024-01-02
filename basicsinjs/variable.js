f4b3729456e8ac530711d9f7afa0fa73c87b99cf
NAVEENSINGH1414 <155405364+NAVEENSINGH1414@users.noreply.github.com>
2024-01-02 04:26:01 +0000

    container config done

---
+++ basicsinjs/variable.js
@@ -0,0 +1,17 @@
+const accountId = 1414//value never change in future//
+let accountEmail = "parihar@google.com"
+var accountPassword ="12345"
+let accountState;
+accountCity = "Rewa"
+
+// accountPassword ="54321"
+// accountEmail = "parihar@microsoft.com"
+// accountCity = "Raipur Karchuliyan"
+// accountId = 2//not allowed //
+console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountCity);
+// console.log(accountPassword);
+
+console.log([accountId,accountEmail,accountPassword,accountCity]);
+console.table([accountId,accountEmail,accountCity,accountPassword,accountCity,accountState]);
