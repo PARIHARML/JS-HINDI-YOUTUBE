c4873ca6be4c52bffec2392ad3a0b99312f408ee
NAVEENSINGH1414 <155405364+NAVEENSINGH1414@users.noreply.github.com>
2024-01-02 05:28:48 +0000

    cantainer config done

--- basicsinjs/variable.js
+++ basicsinjs/variable.js
@@ -3,6 +3,7 @@ let accountEmail = "parihar@google.com"
 var accountPassword ="12345"
 let accountState;
 accountCity = "Rewa"
+accountCity = "Raipur Karchuliyan"
 
 // accountPassword ="54321"
 // accountEmail = "parihar@microsoft.com"
@@ -12,6 +13,11 @@ console.log(accountId);
 // console.log(accountEmail);
 // console.log(accountCity);
 // console.log(accountPassword);
+
+/*
+prefer not use to var 
+because if issue in block scope and functional scope
+*/
 
 console.log([accountId,accountEmail,accountPassword,accountCity]);
 console.table([accountId,accountEmail,accountCity,accountPassword,accountCity,accountState]);
