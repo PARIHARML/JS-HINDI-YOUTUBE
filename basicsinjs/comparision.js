57f1ae9bfc975fd9feee80e57706baf5f43c95f5
Naveen Singh Parihar <155405364+NAVEENSINGH1414@users.noreply.github.com>
2024-01-04 03:00:40 +0000

    comparision

--- basicsinjs/comparision.js
+++ basicsinjs/comparision.js
@@ -1,5 +1,26 @@
-console.log(2 > 1);
-console.log(2 >= 1);
-console.log(2 < 1);
-console.log(2 < 1);
-console.log(2 > 1);
+// console.log("OUTPUT =",2 > 1);
+// console.log("OUTPUT =",2 >= 1);
+// console.log("OUTPUT =",2 < 1);
+// console.log("OUTPUT =",2 <= 1);
+// console.log("OUTPUT =",2 > 1);
+// console.log("OUTPUT =",2 == 1);
+// console.log("OUTPUT =",2 != 1);
+
+// console.log("OUTPUT =","2" >1);
+// console.log("OUTPUT =","02" >1);
+// console.log("OUTPUT =",null > 0);
+// console.log("OUTPUT =",null == 0);//
+// console.log("OUTPUT =",null >= 0);//in comparision null treated as a zero 0; thats y null >= 0 is true but not in the equlity check(==) thats y null ==0 is false//
+
+// console.log("OUTPUT =",undefined >= 0);
+// console.log("OUTPUT =",undefined > 0);
+// console.log("OUTPUT =",undefined == 0);
+// console.log("OUTPUT =",undefined < 0);
+// console.log("OUTPUT =",undefined <= 0);
+// console.log("OUTPUT =",undefined != 0);
+
+//==  this equality check operator not checks strictly along with data type infact it neglect data type
+//===  this equality check operator checks strictly along with data type
+
+console.log("OUTPUT =","2" === 1 );
+console.log("OUTPUT =","2" === 2 );
