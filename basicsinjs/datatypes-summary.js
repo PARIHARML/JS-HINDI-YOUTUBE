11cf7db39738ccb238c87487f9fefe2334db9a8b
Naveen Singh Parihar <155405364+NAVEENSINGH1414@users.noreply.github.com>
2024-01-05 08:28:18 +0000

    string

--- basicsinjs/datatypes-summary.js
+++ basicsinjs/datatypes-summary.js
@@ -36,3 +36,33 @@ const myFunction = function(naagraj) {
 console.log("OUTPUT =",typeof myFunction);
 console.log("OUTPUT =",typeof bigNum);
 console.log("OUTPUT =",typeof Object);
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//TYPES OF MEMORY
+// (1.) STACK("IT USES TO STORE PRIMITIVE DATA-TYPES"),
+    //   => jab v stack memory use hoti hai, Yaha m ne jo V variable declear kiya h uska copy ek milta hai
+
+// (2.)  HEAP("IT USES TO STORE NON-PRIMITIVE DATA-TYPES")
+//   => jab v Heap memory use hoti hai. So Yaha m ne jo V variable declear kiya h uska original reference  milta hai matlab original value reference milta H, So jab hm koi v change karege Vo original value mE change hoga//
+
+let myYoutubename = "Hiteshchoudharydotcom"
+
+let anothername   = myYoutubename;
+anothername       = "ChaiAurCode"
+
+console.log(["OUTPUT AAYEGA =", myYoutubename]);
+console.log(["OUTPUT AAYEGA =", anothername]);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "1414@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email ="Naveenpariahr@google.com"//objeCt ki value ko hm dot(.) 
+                                        //laga kar access karte hain 
+
+console.log("OUTPUT AAYEGA =",userOne.email);
+console.log("OUTPUT AAYEGA =",userTwo.email);
